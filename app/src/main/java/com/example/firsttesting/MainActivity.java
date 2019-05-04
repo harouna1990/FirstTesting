90caab55f81a8b788c734292c410afb3a3224e83
cdac <harounamadougous@gmail.com>
2019-05-03 17:09:15 -0700

    initial commit

---
+++ app/src/main/java/com/example/firsttesting/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.firsttesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
